7822a0bfaf94ee6e4f8d1942b0868c067ef9c3fa
Andrew2608 <andreaboa26@gmail.com>
2025-04-10 15:55:50 +0200

    added yellow treshold and revised rectangles' labels

--- main.cpp
+++ main.cpp
@@ -14,17 +14,15 @@ Scalar highRed1(10, 255, 255);
 Scalar lowRed2(160, 160, 160);
 Scalar highRed2(179, 255, 255);
 
-Scalar lowYellow(160, 160, 160);
-Scalar highYellow(179, 255, 255);
-
-//238 151 72
+Scalar lowYellow(10, 150, 150);
+Scalar highYellow(25, 255, 255);
 
 // defining label colours
 Scalar redLabel(0, 0, 255);
 Scalar blueLabel(255, 0, 0);
-Scalar yellowLabel(255, 215, 0);
+Scalar yellowLabel(0, 255, 255);
 
-void printBoundaries(Mat& cannyMat, Scalar colorRect, Mat& outputMat, vector<vector<Point>>& contours) {
+void printBoundaries(Mat& cannyMat, Scalar colorRect, string labelName, Mat& outputMat, vector<vector<Point>>& contours) {
     findContours(cannyMat, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
     for(const auto& cnt : contours) {
         vector<Point> approx;
@@ -36,14 +34,13 @@ void printBoundaries(Mat& cannyMat, Scalar colorRect, Mat& outputMat, vector<vec
             int w = boundingRect.width;
             int h = boundingRect.height;
             rectangle(outputMat, Point(x, y), Point(x + w, y + h), colorRect, 2);
-            putText(outputMat, "color", Point(x, y), FONT_HERSHEY_PLAIN, 1, colorRect, 2);
+            putText(outputMat, labelName, Point(x, y), FONT_HERSHEY_PLAIN, 1, colorRect, 2);
         }
     }
     contours.clear();
 }
 
 void processColorMask(Mat& colorTreshold, Mat& kernel, int kSizeA, int kSizeB, int dilateIterations, int erodeIterations, int cannyAperture, Mat& outputCanny) {
-    imshow("pre-processing", colorTreshold);
     kernel = getStructuringElement(MORPH_RECT, Size(kSizeA, kSizeB));
     dilate(colorTreshold, colorTreshold, kernel, Point(-1, -1), dilateIterations, MORPH_ELLIPSE);
     erode(colorTreshold, colorTreshold, kernel, Point(-1, -1), erodeIterations, MORPH_ELLIPSE);
@@ -69,37 +66,20 @@ int main() {
     vector<vector<Point>> contours;
     Mat treshold, kernel, smoothed, output = read1.clone();
 
-    /*
-    Mat hsv, colorTreshOutput, output = read1.clone();
-    cvtColor(read1, hsv, COLOR_BGR2HSV);
-
-    Mat treshLow, treshUp, treshold, treshBlue, smoothed;
-    Mat kernel;
-    inRange(hsv, lowRed1, lowRed2, treshLow);
-    inRange(hsv, upRed1, upRed2, treshUp);
-    inRange(hsv, lowBlue, upBlue, treshBlue);
-
-    bitwise_or(treshLow, treshUp, treshold);
-    bitwise_or(treshold, treshBlue, treshold);
-    */
-
     extractColorMask(read1, lowYellow, highYellow, treshold);
     processColorMask(treshold, kernel, 3, 3, 11, 8, 7, smoothed);
-    printBoundaries(smoothed, yellowLabel, output, contours);
+    printBoundaries(smoothed, yellowLabel, "Yellow", output, contours);
 
-    /*
     extractColorMask(read1, lowRed1, highRed1, treshold, lowRed2, highRed2);
     processColorMask(treshold, kernel, 3, 3, 11, 8, 7, smoothed);
-    printBoundaries(smoothed, redLabel, output, contours);
+    printBoundaries(smoothed, redLabel, "Red", output, contours);
 
     extractColorMask(read1, lowBlue, upBlue, treshold);
     processColorMask(treshold, kernel, 3, 3, 11, 8, 7, smoothed);
-    printBoundaries(smoothed, blueLabel, output, contours);
-    */
+    printBoundaries(smoothed, blueLabel,"Blue", output, contours);
 
-    //imshow("original", read1);
-    //imshow("smoothed", smoothed);
-    //imshow("contourned", output);
+    imshow("smoothed", smoothed);
+    imshow("contourned", output);
     waitKey(0);
 
     return 0;
